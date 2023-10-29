80d3458e09a4187c90448a677eca75f98fc18938
Grigor Khachatryan <grigor91khachatryan@gmail.com>
2023-10-30 00:10:22 +0400

    Add project files.

---
+++ AsyncAwait/AsyncDemoForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace AsyncAwait
+{
+    partial class AsyncDemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(23, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(399, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Synchron";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(188, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(500, 42);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(476, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(399, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Async Await";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // AsyncDemoForm
+            // 
+            this.ClientSize = new System.Drawing.Size(918, 308);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "AsyncDemoForm";
+            this.Text = "Factorial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AsyncAwait/AsyncDemoForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace AsyncAwait
+{
+    public partial class AsyncDemoForm : Form
+    {
+
+        public AsyncDemoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Replace("\r\n", "");
+            int number = Convert.ToInt32(textBox1.Text);
+            var result = Factorial(number);
+            textBox1.Text = result.ToString();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Replace("\r\n", "");
+            int number = Convert.ToInt32(textBox1.Text);
+            var result =  await Task<int>.Factory.StartNew(Factorial, number);
+            textBox1.Text = result.ToString();
+        }
+
+        private int Factorial(object obj)
+        {
+            Thread.Sleep(2000);
+            int number = (int)obj;
+            if (number == 0)
+                return 1;
+            return number * Factorial(--number);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verify that the pressed key isn't CTRL or any non-numeric digit
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            // If you want, you can allow decimal (float) numbers
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
